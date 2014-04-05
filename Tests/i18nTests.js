857d55a6c4717721c9be53a05c64e2fc35df4f79
François-Olivier Leblanc <francoisolivier.leblanc@gmail.com>
2014-04-05 15:11:54 -0400

    tests completed

--- Tests/i18nTests.js
+++ Tests/i18nTests.js
@@ -12,11 +12,23 @@ $.getJSON("../resources/lang.json", function(json) {
 
 	i18n.init("fr-CA", json);
 	
-	var translatedString = i18n.translate("GRISOUscientificcontributionsreporterbyVOGG2013");
-	console.log("French for key (GRISOUscientificcontributionsreporterbyVOGG2013): " + translatedString);
+	var translatedString = i18n.translate("grisouscientificcontributionsreporterbyvogg2013");
+	console.log("French for key (grisouscientificcontributionsreporterbyvogg2013): " + translatedString);
 
 	var translatedString = i18n.translate("nonexistingKey");
-	console.log("French for non-existing key (nonexistingKey) should be null: ")
+	console.log("French for non-existing key (nonexistingKey) should be null: " + translatedString);
+});
+
+
+$.getJSON("../resources/lang.json", function(json) {
+
+	i18n.init("en-CA", json);
+	
+	var translatedString = i18n.translate("grisouscientificcontributionsreporterbyvogg2013");
+	console.log("English for key (grisouscientificcontributionsreporterbyvogg2013): " + translatedString);
+
+	var translatedString = i18n.translate("nonexistingKey");
+	console.log("English for non-existing key (nonexistingKey) should be null: " + translatedString);
 });
 
 
