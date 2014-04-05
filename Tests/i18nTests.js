26245c71dcfa6a230f71bebbe6c2f4ec32708b42
François-Olivier Leblanc <francoisolivier.leblanc@gmail.com>
2014-04-05 14:33:32 -0400

    tests added, with html file to run javascript

--- Tests/i18nTests.js
+++ Tests/i18nTests.js
@@ -1,2 +1,39 @@
-var translate = i18n();
-translate.translatePage($(body));
+/*
+$(window).load(function() {
+      alert("window load occurred!");
+});
+*/
+
+$(document).ready(function() {
+/*       alert("document ready occurred!"); */
+
+
+$.getJSON("../resources/lang.json", function(json) {
+
+	i18n.init("fr-CA", json);
+	
+	var translatedString = i18n.translate("GRISOUscientificcontributionsreporterbyVOGG2013");
+	console.log("French for key (GRISOUscientificcontributionsreporterbyVOGG2013): " + translatedString);
+
+	var translatedString = i18n.translate("nonexistingKey");
+	console.log("French for non-existing key (nonexistingKey) should be null: ")
+});
+
+
+
+
+});
+
+function translateToFrench() {
+	$.getJSON("../resources/lang.json", function(json) {
+		i18n.init("fr-CA", json);
+		i18n.translatePage($);
+	});
+}
+
+function translateToEnglish() {
+	$.getJSON("../resources/lang.json", function(json) {
+		i18n.init("en-CA", json);
+		i18n.translatePage($);
+	});
+}
